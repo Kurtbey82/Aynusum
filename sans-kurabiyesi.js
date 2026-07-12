e1dabcbec65fcbf4fa845690400d69fd5607f7ea
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-12 11:29:08 +0300

    Update sans-kurabiyesi.js

--- sans-kurabiyesi.js
+++ sans-kurabiyesi.js
@@ -1,1 +1,19 @@
+const cookie=document.getElementById("cookie");
 
+cookie.addEventListener("click",()=>{
+
+if(navigator.vibrate){
+
+navigator.vibrate(40);
+
+}
+
+cookie.classList.add("shake");
+
+setTimeout(()=>{
+
+cookie.classList.remove("shake");
+
+},550);
+
+});

1c6b0d93f1e69b188f2d69d4a343b769a7c3dced
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-13 10:56:06 +0300

    Update turkiye.js

--- turkiye.js
+++ turkiye.js
@@ -1,13 +1,38 @@
-const cities = document.querySelectorAll(".city");
+window.onload = function () {
 
-cities.forEach(city => {
-    city.addEventListener("click", () => {
+    const svgObject = document.getElementById("turkiyeMap");
 
-        const page = city.dataset.page;
+    svgObject.addEventListener("load", function () {
 
-        if(page){
-            window.location.href = page;
+        const svg = svgObject.contentDocument;
+
+        // Hatay
+        const hatay = svg.getElementById("Hatay");
+
+        // Mersin
+        const mersin = svg.getElementById("Mersin");
+
+        // Isparta
+        const isparta = svg.getElementById("Isparta");
+
+        if(hatay){
+            hatay.style.fill="#ff4d8d";
+            hatay.style.cursor="pointer";
+            hatay.onclick=()=>location.href="hatay.html";
+        }
+
+        if(mersin){
+            mersin.style.fill="#ff4d8d";
+            mersin.style.cursor="pointer";
+            mersin.onclick=()=>location.href="mersin.html";
+        }
+
+        if(isparta){
+            isparta.style.fill="#ff4d8d";
+            isparta.style.cursor="pointer";
+            isparta.onclick=()=>location.href="isparta.html";
         }
 
     });
-});
+
+};

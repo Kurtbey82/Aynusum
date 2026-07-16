6147146e5dcb515fccf866ad8ab0a0db0f7e431f
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-16 14:59:15 +0300

    Update turkiye.js

--- turkiye.js
+++ turkiye.js
@@ -5,44 +5,9 @@ window.onload = function () {
     object.addEventListener("load", function () {
 
         const svg = object.contentDocument;
-        const paths = svg.querySelectorAll("path");
 
-paths.forEach(path => {
-    console.log(path.id);
-});
-
-        const hatay = svg.getElementById("TR31");
-        const mersin = svg.getElementById("TR33");
-        const isparta = svg.getElementById("TR32");
-
-        setupCity(hatay, "Hatay ❤️");
-        setupCity(mersin, "Mersin ❤️");
-        setupCity(isparta, "Isparta ❤️");
+        alert(svg.getElementById("TR31"));
 
     });
 
 };
-
-function setupCity(city, message){
-
-    if(!city){
-        console.log("Şehir bulunamadı");
-        return;
-    }
-
-    city.style.cursor="pointer";
-    city.style.fill="#ff4f8b";
-
-    city.addEventListener("click",function(){
-        alert(message);
-    });
-
-    city.addEventListener("mouseover",function(){
-        city.style.fill="#ff0055";
-    });
-
-    city.addEventListener("mouseout",function(){
-        city.style.fill="#ff4f8b";
-    });
-
-}

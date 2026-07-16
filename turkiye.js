ca64d508f779f35695a6ba5bd035f49ab9011053
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-16 15:14:52 +0300

    Update turkiye.js

--- turkiye.js
+++ turkiye.js
@@ -1,30 +1,19 @@
 window.onload = function () {
 
-    alert("1");
-
     const object = document.getElementById("turkiyeMap");
 
     setTimeout(function () {
 
-        alert("2");
-
         const svg = object.contentDocument;
-
-        if (!svg) {
-            alert("contentDocument = null");
-            return;
-        }
-
-        alert("SVG bulundu");
 
         const hatay = svg.getElementById("TR31");
 
-        if (!hatay) {
-            alert("TR31 bulunamadı");
-        } else {
-            alert("TR31 bulundu");
-            hatay.style.fill = "red";
-        }
+        hatay.style.fill = "red";
+        hatay.style.cursor = "pointer";
+
+        hatay.addEventListener("click", function () {
+            alert("Hatay ❤️");
+        });
 
     }, 1000);
 

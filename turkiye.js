8b27e5837d1317ffd3d5d41cae8cb503a708ad07
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-20 23:33:42 +0300

    Update turkiye.js

--- turkiye.js
+++ turkiye.js
@@ -1,5 +1,15 @@
 window.onload = function () {
 
-    alert("turkiye.js çalıştı");
+    const object = document.getElementById("turkiyeMap");
+
+    object.addEventListener("load", function () {
+
+        const svg = object.contentDocument;
+
+        alert(svg ? "SVG yüklendi" : "SVG yüklenmedi");
+
+        alert(svg.getElementById("TR31"));
+
+    });
 
 };

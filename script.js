d5901e2acd2298f4eee11c23dadf4c12f7c5628a
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-07-25 13:36:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -266,3 +266,14 @@ function showPage(id){
     document.getElementById(id).classList.add("active");
 
 }
+function openCookie(){
+
+    closeMenu();
+
+    setTimeout(function(){
+
+        showPage("cookieSection");
+
+    },200);
+
+}

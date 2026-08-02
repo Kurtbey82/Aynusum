4cf387bc4a000aef3e89b810d8053ea001701cac
Kurtbey82 <kurtmehmet2018@gmail.com>
2026-08-02 10:14:24 +0300

    Create not-detay.js

---
+++ not-detay.js
@@ -0,0 +1,45 @@
+const params = new URLSearchParams(window.location.search);
+
+const index = params.get("id");
+
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+if(index === null || !notes[index]){
+    location.replace("notlar.html");
+}
+
+const note = notes[index];
+
+document.getElementById("noteTitle").textContent = note.title;
+
+document.getElementById("noteInfo").textContent =
+"📅 " + note.date + "   ❤️ " + note.author;
+
+document.getElementById("noteContent").textContent =
+note.content;
+
+document.getElementById("backBtn").onclick = function(){
+
+    location.href = "notlar.html";
+
+};
+
+document.getElementById("deleteBtn").onclick = function(){
+
+    if(confirm("Bu not silinsin mi?")){
+
+        notes.splice(index,1);
+
+        localStorage.setItem("notes",JSON.stringify(notes));
+
+        location.href="notlar.html";
+
+    }
+
+};
+
+document.getElementById("editBtn").onclick = function(){
+
+    alert("Düzenleme ekranını bir sonraki adımda yapacağız 😊");
+
+};
